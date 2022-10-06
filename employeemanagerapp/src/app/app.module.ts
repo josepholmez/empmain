45520ac544e929858bc04b002ce655bc77e6936c
JosephO <joe.olmez@gmail.com>
2022-10-06 17:24:40 -0400

    some changes

--- employeemanagerapp/src/app/app.module.ts
+++ employeemanagerapp/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { EmployeeService } from './employee.service';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { EmployeeService } from '../employee/employee.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule, FormsModule
-  ],
-  providers: [EmployeeService],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [EmployeeService]
 })
 export class AppModule { }
---
+++ employeemanagerapp/src/employee/employee.ts
@@ -0,0 +1,14 @@
+/**
+ * This class is an instance of the Employee.java
+ */
+export class Employee {
+    
+    id: number;
+    name: string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+    
+}
